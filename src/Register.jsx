2337dfb7b91d7566e6049fc4cdf7a9487c13f4fc
NIitishKumar <nitishkumar.ng746@gmail.com>
2021-12-31 20:43:14 +0530

    added condition to register page

--- src/Register.jsx
+++ src/Register.jsx
@@ -16,78 +16,89 @@ function Register() {
   };
 
   const handleSubmit = () => {
-    axios.post("https://back-end-blogapp.herokuapp.com/register", userData).then((res) => {
-      if (res.data.status == 1) {
-        const token = window.localStorage.setItem("token", res.data.token);
-        setTimeout(() => {
-          history.push(`/:${res.data.userId}`);
-          window.location.reload();
-        }, 1000);
-      } else {
-        alert(res.data.message);
-      }
-    });
+    console.log(userData["name"]);
+    if (userData["name"] && userData["email"] && userData["password"]) {
+      axios
+        .post("https://back-end-blogapp.herokuapp.com/register", userData)
+        .then((res) => {
+          if (res.data.status == 1) {
+            const token = window.localStorage.setItem("token", res.data.token);
+            setTimeout(() => {
+              history.push(`/:${res.data.userId}`);
+              window.location.reload();
+            }, 1000);
+          } else {
+            alert(res.data.message);
+          }
+        });
+    } else {
+      alert("Please enter all field !");
+    }
   };
 
   return (
     <div>
-    <div className='' style={{margin : '50px'}} >
-      <div class="container">
-          <div class="row justify-content-md-center" >
+      <div className="" style={{ margin: "50px" }}>
+        <div class="container">
+          <div class="row justify-content-md-center">
             <div class="col col-lg-6">
               <h1>Signup to Continue</h1>
-              <br/>
-                  <div className="form-group">
-                    <label for="fullName">Full Name</label>
-                    <input
-                      name="name"
-                      type="email"
-                      id="fullName"
-                      aria-describedby="emailHelp"
-                      class="form-control"
-                      onChange={handleChange}
-                      placeholder="Enter Full Name"
+              <br />
+              <div className="form-group">
+                <label for="fullName">Full Name</label>
+                <input
+                  name="name"
+                  type="email"
+                  id="fullName"
+                  aria-describedby="emailHelp"
+                  class="form-control"
+                  onChange={handleChange}
+                  placeholder="Enter Full Name"
                 ></input>
-                <br/>
-                  </div>
-                  <div className="form-group">
-                    <label for="inputEmail">Email address</label>
-                    <input
-                      name="email"
-                      type="email"
-                      id="inputEmail"
-                      aria-describedby="emailHelp"
-                      class="form-control"
-                      onChange={handleChange}
-                      placeholder="Enter Email"
-                    ></input>
-                    <small id="emailHelp" class="form-text text-muted">
-                      We'll never share your email with anyone else.
-                    </small>
-                    <br/>
-                  </div>
-                  <div className="form-group">
-                    <label for="inputPassword">Enter Password</label>
-                    <input
-                      className="form-control"
-                      type="password"
-                      id="inputPassword"
-                      onChange={handleChange}
-                      name="password"
-                      placeholder="Enter Password"
-                    ></input>
-                          </div>
-                          <br/>
-                  <button type="submit" className="btn btn-primary" onClick={handleSubmit}>
-                    Submit
-                  </button>
-                  <br />
-                  <a href="/">Already have account ?</a>
+                <br />
               </div>
+              <div className="form-group">
+                <label for="inputEmail">Email address</label>
+                <input
+                  name="email"
+                  type="email"
+                  id="inputEmail"
+                  aria-describedby="emailHelp"
+                  class="form-control"
+                  onChange={handleChange}
+                  placeholder="Enter Email"
+                ></input>
+                <small id="emailHelp" class="form-text text-muted">
+                  We'll never share your email with anyone else.
+                </small>
+                <br />
+              </div>
+              <div className="form-group">
+                <label for="inputPassword">Enter Password</label>
+                <input
+                  className="form-control"
+                  type="password"
+                  id="inputPassword"
+                  onChange={handleChange}
+                  name="password"
+                  placeholder="Enter Password"
+                ></input>
+              </div>
+              <br />
+              <button
+                type="submit"
+                className="btn btn-primary"
+                onClick={handleSubmit}
+              >
+                Submit
+              </button>
+              <br />
+              <a href="/">Already have account ?</a>
+            </div>
           </div>
         </div>
       </div>
-      </div>
+    </div>
   );
 }
 
